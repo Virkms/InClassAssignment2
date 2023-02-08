c9e2691448fd6663d3dc6bf1fa528a42d5587adb
sukhm <sukhm@DESKTOP-GOLDU2O>
2023-02-08 18:01:27 -0500

    In Class Assignment 2. Create an Employee Class, with first name, last name, salary, 10% raise salary data types.

--- src/main/java/EmployeeTest.java
+++ src/main/java/EmployeeTest.java
@@ -10,6 +10,7 @@ import virk.manpreet.employee.Employee;
  *
  * @author sukhm
  */
+import java.util.Scanner;
 public class EmployeeTest {
     public static void main(String[] args) {
         Employee emp1 = new Employee("John" , "Smith", 100000.00);
@@ -22,8 +23,20 @@ public class EmployeeTest {
        
         System.out.printf( "Yearly salary of %s %s:%.2f\n", emp1.getFirstName(), emp1.getLastName(),emp1.getRaisedSalary() );
         System.out.printf( "Yearly salary of %s %s: %.2f\n", emp2.getFirstName(), emp2.getLastName(), emp2.getRaisedSalary());
-
-		
+        
+        System.out.print("Enter the amount of numbers you are going to input");
+        Scanner scanner = new Scanner(System.in);
+        int n =scanner.nextInt();
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < n; i++){
+            System.out.print("Enter Integer" + (i+1) +":");
+            int num = scanner.nextInt();
+            min = num;
+        }
+        
+        System.out.println("The minimum of the set of numbers is:"+min);
+        
+        
         
     }
 }
